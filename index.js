8bc47b820eb3c258e0597ba009f5beb33d5d9578
Eva Janoskova <janoskova.evca@gmail.com>
2021-04-30 11:52:25 +0200

    Přidán JavaScript.

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 'use strict';
 
 console.log('funguju');
+
+const product = document.querySelector('#product-image');
+const whiteButton = document.querySelector('.color-option__box--white');
+const blackButton = document.querySelector('.color-option__box--black');
+const redButton = document.querySelector('.color-option__box--red');
+const blueButton = document.querySelector('.color-option__box--blue');
+const colorOption = document.querySelector('color-option');
+
+const changeColor = (event) => {
+  event.target.classList.add('color-option__chosen--product');
+  console.log(event);
+  if (
+    event.target.className ===
+    'color-option__box color-option__box--white color-option__chosen--product'
+  ) {
+    product.style = 'fill: white';
+    blackButton.classList.remove('color-option__chosen--product');
+    redButton.classList.remove('color-option__chosen--product');
+    blueButton.classList.remove('color-option__chosen--product');
+  } else if (
+    event.target.className ===
+    'color-option__box color-option__box--black color-option__chosen--product'
+  ) {
+    product.style = 'fill: black';
+    whiteButton.classList.remove('color-option__chosen--product');
+    redButton.classList.remove('color-option__chosen--product');
+    blueButton.classList.remove('color-option__chosen--product');
+  } else if (
+    event.target.className ===
+    'color-option__box color-option__box--red color-option__chosen--product'
+  ) {
+    product.style = 'fill: #953232';
+    whiteButton.classList.remove('color-option__chosen--product');
+    blackButton.classList.remove('color-option__chosen--product');
+    blueButton.classList.remove('color-option__chosen--product');
+  } else {
+    product.style = 'fill: skyblue';
+    whiteButton.classList.remove('color-option__chosen--product');
+    blackButton.classList.remove('color-option__chosen--product');
+    redButton.classList.remove('color-option__chosen--product');
+  }
+};
+
+const colorButtons = document.querySelectorAll('.color-option__box');
+for (let i = 0; i < colorButtons.length; i++) {
+  colorButtons[i].addEventListener('click', changeColor);
+}
